53f00d0613fea46db5ab4c8258b9fa5f512890e2
vthotausgbc <159222980+vthotausgbc@users.noreply.github.com>
2024-02-17 17:36:29 -0500

    Update test.py

--- jobs/test.py
+++ jobs/test.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 import snowflake.connector
 import json
-import datetime
 import pymongo
+import pandas as pd
 import pytz
+
 print("Testing Done")
